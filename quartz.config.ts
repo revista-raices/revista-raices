7aafa7aca79226e3ab6ae14f689a1a8d8a3ad23e
revista-raices <raices.antropologiaucr@gmail.com>
2025-12-21 22:58:35 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Lato",
+        body: "Raleway",
         code: "IBM Plex Mono",
       },
       colors: {
