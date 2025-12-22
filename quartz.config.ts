35d016ba2cbd8d95f085cb76d26403792d106b42
revista-raices <raices.antropologiaucr@gmail.com>
2025-12-21 23:27:01 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -37,7 +37,7 @@ const config: QuartzConfig = {
           secondary: "#284b63",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
+          textHighlight: "#646c43",
         },
         darkMode: {
           light: "#161618",
@@ -48,7 +48,7 @@ const config: QuartzConfig = {
           secondary: "#7b97aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#b3aa0288",
+          textHighlight: "#646c43",
         },
       },
     },
