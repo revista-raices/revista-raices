4c8a69eae7932486cb6d1a49bc25ecdec319c918
revista-raices <raices.antropologiaucr@gmail.com>
2025-12-22 16:27:17 -0600

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
+      "Instagram": "https://github.com/jackyzha0/quartz",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
