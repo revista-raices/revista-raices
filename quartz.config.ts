dbd83909de4167f872fabb4c5d405607457e4490
revista-raices <raices.antropologiaucr@gmail.com>
2025-12-21 22:45:49 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Revista Raíces",
+    pageTitleSuffix: "Raíces",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
